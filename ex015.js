5a7894b0d647cb99768b64bff34f2965404b481e
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-08-21 21:31:35 -0300

    the end
    ....

--- ex015.js
+++ ex015.js
@@ -2,25 +2,36 @@ let dia = new Date
 let horas = dia.getHours()
 let url = window.document.getElementById("tempo")
 let imagem2 = window.document.getElementById("flores")
+let header = window.document.getElementById("header")
+let main = window.document.getElementById("main")
 
 if(horas>=6){
     if(horas<12){
         window.document.body.style.backgroundColor = 'yellow'
         imagem2.style.backgroundImage = `url(imagens/pexels-neosiam-707571.jpg)`
+        header.style.backgroundColor = 'yellow'
+        main.style.backgroundColor = 'yellow'
     }
     else if(horas>=12){
         if(horas<18){
             window.document.body.style.backgroundColor = 'green'
-            imagem2.style.backgroundImage = `url(imagens/pexels-bill-white-165537\ \(1\).jpg)`
+            imagem2.style.backgroundImage = `url(imagens/arvore.jpg)`
+            header.style.backgroundColor = 'green'
+            main.style.backgroundColor = 'green'
+            
         }
-        else{
+        else if(horas>=18){
             window.document.body.style.backgroundColor = 'gray'
             imagem2.style.backgroundImage = `url(imagens/pexels-asad-photo-maldives-1024975.jpg)`
+            header.style.backgroundColor = 'gray'
+            main.style.backgroundColor = 'gray'
         }  
     }
 }
 else{
     window.document.body.style.backgroundColor = 'gray'
     imagem2.style.backgroundImage = `url(imagens/pexels-asad-photo-maldives-1024975.jpg)`
+    header.style.backgroundColor = 'gray'
+    main.style.backgroundColor = 'gray'
 }
 url.innerText = `Agora é ${horas} horas`
